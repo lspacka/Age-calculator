b4bf604d04634dfb2bf16fc38c1659a637ea0129
julian <jdrc@protonmail.com>
2024-03-04 18:13:27 -0500

    basic working layout

--- App.js
+++ App.js
@@ -58,7 +58,7 @@ function App() {
         year={year}
         setCalcResult={setCalcResult}
       />
-      <DisplayGroup calcResult={calcResult} />
+      <DisplayGroup calcResult={calcResult} className="display-group"/>
     </>
   )
 }
@@ -67,6 +67,8 @@ function InputGroup({ day, month, year, setDay, setMonth, setYear, setCalcResult
   const [errorMsg1, setErrorMsg1] = React.useState('')
   const [errorMsg2, setErrorMsg2] = React.useState('')
   const [errorMsg3, setErrorMsg3] = React.useState('')
+  const [color, setColor] = React.useState('#716f6f')
+  const [borderColor, setBorderColor] = React.useState('#dbdbdb')
 
   function isLeapYear(year) {
     return (year%4==0 && year%100!=0) || (year%400==0)
@@ -84,10 +86,10 @@ function InputGroup({ day, month, year, setDay, setMonth, setYear, setCalcResult
 
     // validate day 
     if (!day) {
-      setErrorMsg1('This field is required.')
+      setErrorMsg1('This field is required')
       is_valid =  false
-    } else if (isNaN(day) || day < 1 || day > 31) {   // HERE
-      setErrorMsg1('Must be a valid day.')
+    } else if ((day < 1 || day > 31) || isNaN(day)) {   // HERE
+      setErrorMsg1('Must be a valid day')
       is_valid =  false
     } else {
       setErrorMsg1('')
@@ -95,7 +97,7 @@ function InputGroup({ day, month, year, setDay, setMonth, setYear, setCalcResult
 
     // validate month
     if (!month) {
-      setErrorMsg2('This field is required.')
+      setErrorMsg2('This field is required')
       is_valid =  false
     } else if (isNaN(month) || month < 1 || month > 12) {
       setErrorMsg2('Must be a valid month')
@@ -106,10 +108,10 @@ function InputGroup({ day, month, year, setDay, setMonth, setYear, setCalcResult
 
     // validate year
     if (!year) {
-      setErrorMsg3('This field is required.')
+      setErrorMsg3('This field is required')
       is_valid =  false
     } else if (isNaN(year) || year.length != 4 || year < 0) {
-      setErrorMsg3('Must be a valid year.')
+      setErrorMsg3('Must be a valid year')
       is_valid =  false
     } else if (parseInt(year) > current_year) {
       setErrorMsg3('Must be in the past')
@@ -122,6 +124,10 @@ function InputGroup({ day, month, year, setDay, setMonth, setYear, setCalcResult
   }
 
   function isValidDate(day, month, year) {
+    // attempt to fix that mf day display bug
+    // if (day > 31 && !month && !year) 
+    //   return false   
+
     if (month==2) {
       if ((!isLeapYear(year) && day>28) || (isLeapYear(year) && day>29)) 
         return false
@@ -138,6 +144,7 @@ function InputGroup({ day, month, year, setDay, setMonth, setYear, setCalcResult
     
     return true
   }
+
 
   const handleDayChange = (e) => {
     setDay(e.target.value)
@@ -159,86 +166,65 @@ function InputGroup({ day, month, year, setDay, setMonth, setYear, setCalcResult
     // let current_year = new Date().getFullYear()
     let is_valid = true
 
-    // // validate day 
-    // if (!day) {
-    //   setErrorMsg1('This field is required.')
-    //   is_valid = false
-    // } else if (isNaN(day) || day < 1 || day > 31) {   // HERE
-    //   setErrorMsg1('Must be a valid day.')
-    //   is_valid = false
-    // } else {
-    //   setErrorMsg1('')
-    // }
-
-    // // validate month
-    // if (!month) {
-    //   setErrorMsg2('This field is required.')
-    //   is_valid = false
-    // } else if (isNaN(month) || month < 1 || month > 12) {
-    //   setErrorMsg2('Must be a valid month')
-    //   is_valid = false
-    // } else {
-    //   setErrorMsg2('')
-    // }
-
-    // // validate year
-    // if (!year) {
-    //   setErrorMsg3('This field is required.')
-    //   is_valid = false
-    // } else if (isNaN(year) || year.length != 4 || year < 0) {
-    //   setErrorMsg3('Must be a valid year.')
-    //   is_valid = false
-    // } else if (parseInt(year) > current_year) {
-    //   setErrorMsg3('Must be in the past')
-    //   is_valid = false
-    // } else {
-    //   setErrorMsg3('')
-    // }
-
-    // if (!validateInputs(day, month, year)) {
-    //   is_valid = false
-    //   setCalcResult({})
-    // }
     is_valid = validateInputs(day, month, year)
-    if (!is_valid) setCalcResult({})
+    if (!is_valid) {
+      setCalcResult({})
+      setColor('#ff5757')
+      setBorderColor('#ff5757')
+    } 
   
     //  validate date
     if (!isValidDate(day, month, year)) {
       setCalcResult({})
+      // maybe try a sep container for 'invalid date' msg
+      setColor('#ff5757')
+      InvalidDate()
       is_valid = false
-      console.log('invalid date')
-      // InvalidDate()
     }
 
     //  do calc
     if (is_valid) {
-      console.log('Valid date entered, performing calculations...')
+      setColor('#716f6f')
+      setBorderColor('#dbdbdb')
       const birthDate = `${year}-${month}-${day}`;
       const result = calcAge(birthDate)
       setCalcResult(result)
     }
   }
 
+  const handleFocus = () => {
+    setBorderColor('#854dff')
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <div className="input-group">
-        <Input 
-          label="DAY" 
+        <Input
+          borderColor={borderColor}
+          color={color} 
+          label="D A Y" 
           placeholder="DD" 
           errorMsg={errorMsg1}
           onChange={handleDayChange}
+          onFocus={handleFocus}
         />
-        <Input 
-          label="MONTH" 
+        <Input
+          borderColor={borderColor}
+          color={color} 
+          label="M O N T H" 
           placeholder="MM" 
           errorMsg={errorMsg2}  
           onChange={handleMonthChange}
+          onFocus={handleFocus}
         />
         <Input 
-          label="YEAR" 
+          borderColor={borderColor}
+          color={color}
+          label="Y E A R" 
           placeholder="YYYY" 
           errorMsg={errorMsg3} 
           onChange={handleYearChange}
+          onFocus={handleFocus}
         />
         <SubmitButton />
       </div>
@@ -249,16 +235,18 @@ function InputGroup({ day, month, year, setDay, setMonth, setYear, setCalcResult
   )
 }
 
-function Input({ label, placeholder, errorMsg, onChange }) {
+function Input({ color, borderColor, label, placeholder, errorMsg, onChange, onFocus }) {
   return (
     <div className="input-box ">
-      <label htmlFor={label}>{label}</label>
+      <label htmlFor={label} style={{ color: color }}>{label}</label>
       <input 
+        style={{ borderColor: borderColor }}
         type="text" 
         placeholder={placeholder}
         className="input" 
         name={label}
         onChange={onChange}
+        onFocus={onFocus}
         // autoComplete="off"
       >
       </input>
@@ -269,18 +257,18 @@ function Input({ label, placeholder, errorMsg, onChange }) {
 
 function DisplayGroup({ calcResult }) {  
   return (
-    <>
-      <Display time="Years" value={calcResult.diffYears} />
-      <Display time="Months" value={calcResult.diffMonths} />
-      <Display time="Days" value={calcResult.diffDays} />
-    </>
+    <div className="display-group">
+      <Display time="years" value={calcResult.diffYears} />
+      <Display time="months" value={calcResult.diffMonths} />
+      <Display time="days" value={calcResult.diffDays} />
+    </div>
   )
 }
 
 function Display({ time, value }) {
   if (!value && value!=0) value = '--'
 
-  return <div className="display">{value} {time}</div>
+  return <div className="display"><span>{value}</span> {time}</div>
 }
 
 function SubmitButton() {
